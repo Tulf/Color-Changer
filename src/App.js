d0230ec550ce05fd8cd4f6c74dcf1fb9111b0379
tulf <timzulf@gmail.com>
2022-01-06 12:38:08 -0500

    initial blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const originalColors = ["#ff0000", "#0000ff", "	#00ff00", "#ffff00"];
+  const [colors, setColors] = useState(originalColors);
+  const changeColor = (i) => {
+    const getRandomColor = () =>
+      `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    let randomColor = getRandomColor();
+    while (randomColor === originalColors[i]) {
+      randomColor = getRandomColor();
+    }
+    let newColors = [...colors];
+    newColors[i] =
+      newColors[i] === originalColors[i] ? randomColor : originalColors[i];
+    setColors(newColors);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {colors.map((a, i) => (
+        <div
+          key={a}
+          style={{ backgroundColor: a, height: "100px", width: "100px" }}
+          id={a}
+          onClick={() => changeColor(i)}
+        ></div>
+      ))}
     </div>
   );
 }
 
-export default App;
+/*
+click color
+triggers different div change color
+fire timer to clicking the other div
+  [recentlyClicked, setRecentlyClicked] = useState()
+  [recentlyBlackened , setRecentlyBlackened] = useState()
+  const timeout, if exceeded set game over to true
+  clear timeout
+  [game, setGameOver] if true return gameover
+  Useeffect(to check recnetly clicked vs blackened)
+  clears timeout
+  
+  if game over have a conditonal return
+*/
